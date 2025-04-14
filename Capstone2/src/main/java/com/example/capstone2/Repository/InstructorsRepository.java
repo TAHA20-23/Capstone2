0323dbf0b382ffd90cd2fdbf458fd8109fb139b9
TAHA20-23 <taha.yab2o2o@gmail.com>
2025-04-14 21:13:31 +0300

    Update InstructorsRepository.java

--- Capstone2/src/main/java/com/example/capstone2/Repository/InstructorsRepository.java
+++ Capstone2/src/main/java/com/example/capstone2/Repository/InstructorsRepository.java
@@ -1,12 +1,17 @@
 package com.example.capstone2.Repository;
 
 import com.example.capstone2.Model.Instructors;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InstructorsRepository extends JpaRepository<Instructors, Integer> {
 
     Instructors findInstructorsById(Integer id);
+
+    List<Instructors> findInstructorsByDepartmentId(Integer departmentId);
 
 }
